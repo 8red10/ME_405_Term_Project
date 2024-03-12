22a2cc6909d6652eba1f994acf4e51d696fbbb28
8red10 <22091223+8red10@users.noreply.github.com>
2024-03-12 02:18:17 -0700

    camera test update

--- src/test.py
+++ src/test.py
@@ -5,7 +5,8 @@ A file to help test other files and other code
 
 '''
 
-import pyb 
+# import pyb 
+from machine import I2C
 import utime
 import mlx_cam
 
@@ -17,7 +18,7 @@ def test_camera_nonblock():
     @returns    None.
     '''
     # initialize a camera object
-    camera = mlx_cam.MLX_Cam(i2c=pyb.I2C(2))
+    camera = mlx_cam.MLX_Cam(i2c=I2C(1))
     # set and report the refresh rate
     print(f"Current refresh rate: {camera._camera.refresh_rate}")
     camera._camera.refresh_rate = 10.0
@@ -26,10 +27,10 @@ def test_camera_nonblock():
     image = None
     # initialize the comparison time
     start_time = utime.ticks_ms()
-    # # repeatedly try to get image
-    # while not image:
-    image = camera.get_image_nonblocking()
-    print(f'time to check image = {utime.ticks_diff(utime.ticks_ms(),start_time)} ms')
+    # repeatedly try to get image
+    while not image:
+        image = camera.get_image_nonblocking() # doing this once takes about 157ms
+        print(f'time to check image = {utime.ticks_diff(utime.ticks_ms(),start_time)} ms')
     # indicate done with this test
     print('Done testing image nonblock time.')
 
