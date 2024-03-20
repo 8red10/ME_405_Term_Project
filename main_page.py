74ce704a96666fe933d1209b2825e34ee2be2d9f
8red10 <22091223+8red10@users.noreply.github.com>
2024-03-19 23:25:27 -0700

    add main page

---
+++ main_page.py
@@ -0,0 +1,21 @@
+'''!
+@mainpage title of mainpage ?
+details
+
+here
+
+
+are more
+
+things too. 
+
+
+need to add subsections and details
+TODO
+
+
+
+here's an image!
+
+\image html ./src/images/task_diagram.png 'task_diagram' height=300
+'''
