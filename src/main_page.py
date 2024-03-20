612ffaa4201dbf6efe872efe574e7dcc1e284220
8red10 <22091223+8red10@users.noreply.github.com>
2024-03-20 00:35:41 -0700

    main page update

---
+++ src/main_page.py
@@ -0,0 +1,23 @@
+'''!
+@mainpage title of mainpage 
+details
+
+here
+
+
+are more
+
+things too. 
+
+
+need to add subsections and details
+TODO
+
+
+another attempt\n
+<img src='./../src/images/button_state_diagram.png' alt='button state diagram' height='300'>
+
+svg attempt
+@image html image_state_diagram.svg 'image' height=300
+d
+'''
